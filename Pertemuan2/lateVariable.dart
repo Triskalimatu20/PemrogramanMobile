05770850df5e7c783743a17f6b32e24e9935f094
Triskalimatu20 <triskalimatu@gmail.com>
2026-02-26 13:19:40 +0700

    Pertemuan 2

---
+++ Pertemuan2/lateVariable.dart
@@ -0,0 +1,6 @@
+void main() {
+  late String nama;
+
+  nama = "Triska";
+  print(nama);
+}
---
+++ Pertemuan2/soal1_2.dart
@@ -0,0 +1,7 @@
+void main() {
+  String nama = "Triska";
+
+  for (int i = 21; i >= 9; i--) {
+    print("Nama saya adalah $nama, sekarang berumur $i");
+  }
+}
